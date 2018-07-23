4ba3d990b566fdaba2a85b3f13cce7fe830436a4
Magrid Prisca Juwita <magridprisca@gmail.com>
2018-07-23 11:50:04 +0700

    perbaikan

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database','form_validatio','session');
+$autoload['libraries'] = array('database','form_validation','session');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url','form');
 $autoload['config'] = array();
