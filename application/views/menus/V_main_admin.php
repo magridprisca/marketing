34cf17668d18795f9fd66e13fb3688feb87ae7f4
arianidwiwulandari <Ariani_dwiwulandari@yahoo.com>
2018-07-24 17:46:14 +0700

    Update V_main_admin.php

--- application/views/menus/V_main_admin.php
+++ application/views/menus/V_main_admin.php
@@ -1,10 +1,11 @@
 <li class="header">MAIN NAVIGATION</li>
-<li class="active menu-open">
+<li class="active treeview menu-open">
   <a href="#">
     <i class="fa fa-universal-access"></i> <span>Dashboard</span>
+    <span class="pull-right-container">
   </a>
 </li>
-<li class="treeview">
+<li class="treeview menu-open">
   <a href="#">
     <i class="fa fa-dashboard"></i> <span>Project</span>
     <span class="pull-right-container">
@@ -16,7 +17,8 @@
     <li class="active"><a href="<?= base_url('C_Project');?>"><i class="fa fa-circle-o"></i> Process Project</a></li>
   </ul>
 </li>
-<li>
-  <a href="C_client/getList">
+<li class="menu-open">
+  <a href="<?= base_url('C_client/getList');?>">
     <i class="fa fa-handshake-o"></i> <span>Client</span>
+    <span class="pull-right-container">
 </li>
