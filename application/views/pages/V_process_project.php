d519a3eb08beb0406fbe4c2a35952168414873f1
arianidwiwulandari <Ariani_dwiwulandari@yahoo.com>
2018-07-25 12:16:48 +0700

    cuman halaman

---
+++ application/views/pages/V_process_project.php
@@ -0,0 +1,25 @@
+<?php $this->load->view('templates/header')?>
+<section class="content-header">
+  <h1>
+    Client
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-universal-access"></i> Home</a></li>
+    <li>Project</li>
+    <li class="active">Progress Project</li>
+  </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Datatable Client List</h3>
+          <div class="box-tools pull-right">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<section>
