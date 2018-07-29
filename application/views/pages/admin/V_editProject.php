6402daf6e5e9b2d84001965a2a5bcb892b76f4a7
arianidwiwulandari <Ariani_dwiwulandari@yahoo.com>
2018-07-29 22:08:46 +0700

    ?

--- application/views/pages/admin/V_editProject.php
+++ application/views/pages/admin/V_editProject.php
@@ -63,43 +63,37 @@
                             <tr style="background-color: #FFFFFF">
                               <th>1</th>
                               <th colspan="2">Promosi (Market Development)</th>
-                              <th><input type="checkbox"></th>
-                              <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
-                            </tr>
-                            <tr style="background-color: #FFFFFF">
-                              <th>1</th>
-                              <th colspan="2">Promosi (Market Development)</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="mardev"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>2</th>
                               <th colspan="2">Opsreq</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="opsreq"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>3</th>
                               <th colspan="2">Design</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="design"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>4</th>
                               <th colspan="2">Presentasi</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="presentasi"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>5</th>
                               <th colspan="2">Sourcing Harga</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="sourceprice"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>6</th>
                               <th colspan="2">Penyimpanan Dokumen Lelang (SDM, Fasilitas, Finansial)</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="sff"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                           </tbody>
@@ -119,122 +113,122 @@
                             <tr style="background-color: #FFFFFF">
                               <th>7</th>
                               <th colspan="2">Pengumuman Lelang</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pgmL"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>8</th>
                               <th colspan="2">Pendaftaran Lelang</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pdfL"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>9</th>
                               <th>Tahap Prakualifikasi</th>
                               <th>Pemasukan Dokumen Prakualifikasi</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" anme="pmDP"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>10</th>
                               <th>Tahap Prakualifikasi</th>
                               <th>Pembukaan Dokumen Prakualifikasi</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pmbDP"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>11</th>
                               <th>Tahap Prakualifikasi</th>
                               <th>Pembuktian Kualifikasi</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pmbkDP"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>12</th>
                               <th colspan="2">Pengambilan Dokumen Lelang</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pgDL"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>13</th>
                               <th colspan="2">Aanwizjing</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="aanwizjing"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>14</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Pemasukan Dokumen Penawaran Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pmDPAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>15</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Pembukaan Dokumen Penawaran Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pmbDPAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>16</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Evaluasi Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="evalAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>17</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Klarifikasi Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="klarifAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>18</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Negoisasi Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="negoAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>19</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Pengumuman Hasil Administrasi dan Teknis</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pengumumanAT"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>20</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Pemasukan Penawaran Harga</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pemasukanTwr"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>21</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Evaluasi Harga</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="evalHarga"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>22</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Negosiasi Harga</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="negoHarga"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>23</th>
                               <th>Tahap Pemasukan Dokumen</th>
                               <th>Pengumuman Pemenang</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="pengumumanMenang"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>24</th>
                               <th colspan="2">Tim Evaluasi Pengadaan (TEP)</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="TEP"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                           </tbody>
@@ -254,25 +248,25 @@
                             <tr style="background-color: #FFFFFF">
                               <th>25</th>
                               <th colspan="2">SPMK (Penetapan Pemenang)</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="SPMK"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>26</th>
                               <th colspan="2">Pembahasan Kontrak</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="bahasK"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>27</th>
                               <th colspan="2">Penandatanganan Kontrak</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="ttdK"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                             <tr style="background-color: #FFFFFF">
                               <th>28</th>
                               <th colspan="2">Efektif Kontrak</th>
-                              <th><input type="checkbox"></th>
+                              <th><input type="checkbox" name="efektifK"></th>
                               <th><input type="date" value="<?= date('Y-m-d')?>" border=0></th>
                             </tr>
                           </tbody>
