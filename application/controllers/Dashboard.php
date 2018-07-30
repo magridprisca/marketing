38302e1212004f0d721d15bcb7ddb34596f81602
Magrid Prisca Juwita <magridprisca@gmail.com>
2018-07-30 09:04:05 +0700

    dash dikit

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,11 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
-
+    $this->load->model('M_Project');
 	}
 
 	public function index(){
 		$data['menu']='home';
+		$data['project']=$this->M_Project->getcount();
 		$this->load->view('pages/dashboard',$data);
 	}
 }
--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -15,12 +15,12 @@
     <section class="content">
       <!-- Info boxes -->
       <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-gear"></i></span>
+            <span class="info-box-icon bg-aqua"><i class="fa fa-folder-open"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">CPU Traffic</span>
+              <span class="info-box-text">Project</span>
               <span class="info-box-number">90<small>%</small></span>
             </div>
             <!-- /.info-box-content -->
@@ -28,12 +28,12 @@
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+            <span class="info-box-icon bg-red"><i class="fa fa-spinner"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
+              <span class="info-box-text">On Process</span>
               <span class="info-box-number">41,410</span>
             </div>
             <!-- /.info-box-content -->
@@ -45,26 +45,13 @@
         <!-- fix for small devices only -->
         <div class="clearfix visible-sm-block"></div>
 
-        <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="col-md-4 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-cart-plus"></i></span>
+            <span class="info-box-icon bg-green"><i class="fa fa-check-circle"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Sales</span>
+              <span class="info-box-text">Done</span>
               <span class="info-box-number">760</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="fa fa-user"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">New Members</span>
-              <span class="info-box-number">2,000</span>
             </div>
             <!-- /.info-box-content -->
           </div>
